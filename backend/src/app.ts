c667fc0270faab07b9c338b401e7e42dc6e40a46
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-09-02 20:37:09 +0300

    Add mangoose and use it to connect to MongoDB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,24 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+
+const mongoUri = process.env.MONGO_URI ?? "mongodb://localhost:27017/chern";
+
+console.log("Attempting to connect to MongoDB...");
+
+mongoose.connect(mongoUri, {})
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch((err) => {
+    console.error("Failed to connect to MongoDB: ", err);
+});
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+    res.send("Hello World");
 });
 
 const PORT = process.env.PORT ?? 3000;
