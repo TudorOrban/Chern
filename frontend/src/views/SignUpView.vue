3a77907b9fa95b3609e16b844d21d3e2023c6d18
TudorOrban <130213626+TudorOrban@users.noreply.github.com>
2024-09-03 15:59:38 +0300

    Add Sign Up form

--- frontend/src/views/SignUpView.vue
+++ frontend/src/views/SignUpView.vue
@@ -1,21 +1,38 @@
 <template>
-    <div class="flex items-center justify-between w-full px-8 py-4 border-b border-gray-300 shadow-sm">
-        <div>
+    <div class="flex flex-col items-center justify-center w-full py-8">
+        <div class="py-4">
             <h2 class="text-xl font-semibold">Sign Up</h2>
         </div>
         
-        <div class="flex items-center space-x-2">
-            <router-link to="/login">
-                Login
-            </router-link>
+        <form @submit.prevent="handleSubmit" class="form-container" space-y-4>
+            <div class="text-left">
+                <label for="email" class="form-label">Email:</label>
+                <input id="email" v-model="email" @blur="validateEmail" class="form-input" />
+                <span v-if="errors.email" class="form-error">{{ errors.email }}</span>
+            </div>
+            <div class="text-left">
+                <label for="username" class="form-label">Username:</label>
+                <input id="username" v-model="username" @blur="validateUsername" class="form-input" />
+                <span v-if="errors.username" class="form-error">{{ errors.username }}</span>
+            </div>
+            <div class="text-left">
+                <label for="password" class="form-label">Password:</label>
+                <input id="password" v-model="password" @blur="validatePassword" class="form-input" />
+                <span v-if="errors.password" class="form-error">{{ errors.password }}</span>
+            </div>
 
-            <button
-                v-if="!isLoggedIn"
-                @click="signUp"
-            >
-                Sign Up
-            </button>
-        </div>
+            <div class="flex items-center justify-between w-full p-8">
+                <button type="submit" class="form-button">Sign Up</button>
+
+                <router-link to="/sign-up" class="form-link">
+                    Sign Up
+                </router-link>
+            </div>
+
+            <div v-if="areCredentialsInvalid" class="form-message">
+                <p>Invalid email or password.</p>
+            </div>
+        </form>
     </div>
     
 </template>
@@ -27,23 +44,61 @@ import { Vue } from 'vue-class-component';
 export default class SignUpView extends Vue {
     authService!: AuthService;
 
-    isLoggedIn = false;
+    email = '';
+    username = '';
+    password = '';
+    errors = {
+        email: '',
+        username: '',
+        password: ''
+    };
+
+    areCredentialsInvalid = false;
     
     created() {
         this.authService = new AuthService();
     }
+    
+    validateEmail() {
+        if (!this.email.includes('@')) {
+            this.errors.email = 'Please enter a valid email.';
+        } else {
+            this.errors.email = '';
+        }
+    }
 
-    async signUp() {
+    validatePassword() {
+        if (this.password.length < 6) {
+            this.errors.password = 'Password must be at least 6 characters.';
+        } else {
+            this.errors.password = '';
+        }
+    }
+
+    validateUsername() {
+        if (this.username.length < 6) {
+            this.errors.username = 'Username must be at least 6 characters.';
+        } else {
+            this.errors.username = '';
+        }
+    }
+
+    async handleSubmit() {
+        this.validateEmail();
+        this.validatePassword();
+        this.validateUsername();
+        if (this.errors.email || this.errors.username || this.errors.password) {
+            return;
+        }
+
         try {
-            const response = await this.authService.signUp('to3@gmail.com', 'cjql195jsov', 'to3');
-            console.log("Sign up successful: ", response);
+            const response = await this.authService.signUp(this.email, this.password, this.username);
+            console.log("Sign Up successful: ", response);
             localStorage.setItem('userToken', response.token);
-            this.isLoggedIn = true;
         } catch (error) {
-            console.error("Sign up failed: ", error);
+            this.areCredentialsInvalid = true;
+            console.error("Sign Up failed: ", error);
         }
     }
 }
-
-
 </script>
